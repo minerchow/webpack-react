552142c6421547740a927324fe767b2e423cde64
zhouming <minerchow@126.com>
2018-11-06 22:50:58 +0800

    update

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,6 +4,7 @@ import Bottom from './index/bottom';
 import Bodys from './index/bodys';
 import Top from './index/top';
 import CountStore from '../store/countStore';
+import { Picker, List, WhiteSpace } from 'antd-mobile';
 const CountStores  = new CountStore();
 var arr=[1,2,3,4];
 var arrs=[{
@@ -12,17 +13,38 @@ var arrs=[{
 {
     a:"b"
 }]
+let district = [{
+    value:'a',
+    label:'a'
+},
+{
+    value:'b',
+    label:'b'
+}
+]
 class Index extends Component {
     constructor(props){
         super(props);
-       
+        this.state = {
+            pickarr:[]
+        }
+        //this.okChange = this.okChange.bind(this,v)
+    }
+    okChange(v){
+        this.setState({
+            pickarr:v
+        })
     }
     render(){
+        let pickarr = this.state.pickarr
         return <div id="index">
         <div>123132</div>
             <Top id="top" kkk={arr}  dataid={arrs}></Top>
             <Bodys id="bodys" countstore={CountStores}>744855</Bodys>
             <Bottom id="bottom">213213</Bottom>
+            <Picker data={district} cols={1} value={pickarr} className="forss" onOk={v=>this.okChange(v)}>
+                <List.Item arrow="horizontal">Single</List.Item>
+            </Picker>
         </div>
     }
 
